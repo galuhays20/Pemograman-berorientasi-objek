95c0a1b0b6e5769b14afbf60d0bd223ac82ca147
galuhays20 <89930078+galuhays20@users.noreply.github.com>
2022-08-30 00:45:33 +0700

    Add files via upload

---
+++ MyProject2/src/myproject2/MyProject2.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author galuh
+ */
+public class MyProject2 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("      ");        
+        System.out.print("AAAA");
+        System.out.print("         ");        
+        System.out.print("YYY");
+        System.out.print("     ");
+        System.out.print("YYY");
+        System.out.print("     ");          
+        System.out.print("UUU");  
+        System.out.print("      ");          
+        System.out.println("UUU");
+
+        
+        
+        
+        System.out.print("    ");
+        System.out.print("AAA");
+        System.out.print("  ");
+        System.out.print("AAA");
+        System.out.print("        ");                
+        System.out.print("YYY");        
+        System.out.print("   "); 
+        System.out.print("YYY");
+        System.out.print("      ");          
+        System.out.print("UUU");
+        System.out.print("      ");          
+        System.out.println("UUU");
+
+  
+        
+        System.out.print("   ");
+        System.out.print("AAA");
+        System.out.print("    ");
+        System.out.print("AAA");
+        System.out.print("        ");                
+        System.out.print("YYY"); 
+        System.out.print(" ");
+        System.out.print("YYY");
+        System.out.print("       ");
+        System.out.print("UUU"); 
+        System.out.print("      ");          
+        System.out.println("UUU");
+        
+        
+        System.out.print("  ");
+        System.out.print("AAAAAAAAAAAA");
+        System.out.print("        ");                
+        System.out.print("YYYYY"); 
+        System.out.print("        ");                 
+        System.out.print("UUU"); 
+        System.out.print("      ");          
+        System.out.println("UUU");
+                
+        
+        System.out.print(" ");
+        System.out.print("AAA");
+        System.out.print("        ");
+        System.out.print("AAA");
+        System.out.print("        ");                
+        System.out.print("YYY");                 
+        System.out.print("          ");                         
+        System.out.print("UU"); 
+        System.out.print("      ");          
+        System.out.println("UU");
+
+
+                
+        
+        System.out.print("");
+        System.out.print("AAA");
+        System.out.print("          ");
+        System.out.print("AAA");
+        System.out.print("       ");                
+        System.out.print("YYY"); 
+        System.out.print("           ");          
+        System.out.println("UUUUUUUU"); 
+
+    }    
+    }
