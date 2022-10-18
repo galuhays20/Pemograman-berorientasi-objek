6b14218b74fb5d34d637d747e18b2a7eac6cbf39
galuhays20 <89930078+galuhays20@users.noreply.github.com>
2022-10-18 22:24:16 +0700

    Add files via upload

---
+++ ProjectBangunDatar2/src/projectbangundatar2/Lingkaran.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+import static java.lang.Math.PI;
+
+/**
+ *
+ * @author galuh
+ */
+public class Lingkaran {
+    double hitungLuas(int r){
+        double luas = 22/7 * r * r;
+        return luas;
+    }
+    double hitungLuas(double r){
+        double luas = 22/7 * r * r;
+        return luas;
+    }
+    double hitungKelilings(int r){
+        double keliling = 22/7 * r * 2;
+        return keliling;
+    }
+    double hitungKeliling(double r){
+        double keliling = 22/7 * r * 2;
+        return keliling;
+    }
+}
